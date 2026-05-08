018a67ff84a080c390c7dbec8d5f9eb3f387c0ab
Dev Melo <omelodev@gmail.com>
2026-05-08 14:25:20 -0300

    Declaração de Variaveis: var / tipo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var nome = "felipe";
+        int idade = 36;
+        System.out.println("Nome " + nome + ", idade " + idade);
+
+
+    }
+}
