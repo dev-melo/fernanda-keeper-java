92b13e637c629885a625d7b9ccb5ab1e5f5166d0
Dev Melo <omelodev@gmail.com>
2026-05-08 14:40:01 -0300

    condicionais

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // -- Declarar variaveis
+
         var nome = "felipe";
         int idade = 36;
         System.out.println("Nome " + nome + ", idade " + idade);
 
+        // -- Tipos Primitivos
         //Numeros inteiros
         byte num8bits;
         short num16bits;
@@ -21,6 +24,26 @@ public class Main {
         // Boleanos (verdadeiro ou falso)
         boolean verdade = true;
         verdade = false;
+
+        // -- Condicionais
+
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        String s1 = "";
+        boolean bool = false;
+
+        if (bool ){
+            System.out.println("Verdadeiro");
+        } else if (s1.isBlank()) {
+            System.out.println("Esta vazia.");
+        } else {
+            System.out.println("Falso");
+        }
 
 
     }
