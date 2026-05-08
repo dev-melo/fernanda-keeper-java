517e482d785170987bc1b6ab6583d333d296f0b4
Dev Melo <omelodev@gmail.com>
2026-05-08 14:29:05 -0300

    tipos primitivos

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,24 @@ public class Main {
         int idade = 36;
         System.out.println("Nome " + nome + ", idade " + idade);
 
+        //Numeros inteiros
+        byte num8bits;
+        short num16bits;
+        int num32bits;
+        long num64bits;
+
+        // Numeros Decimais
+        float numDecimal32bits; // precisão simples
+        double numDecimal64bits; // precisão dupla
+
+        // Caracteres, Palavras, Frases
+        char sexo = 'M'; // unico caracter
+        String nomeFilho = "Iori"; // palavras e frases
+
+        // Boleanos (verdadeiro ou falso)
+        boolean verdade = true;
+        verdade = false;
+
 
     }
 }
